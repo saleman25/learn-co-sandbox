2f34deed77b39617afada2c7308fbeeb636bc586
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-19 00:04:46 +0000

    added to the api class

--- project/lib/api.rb
+++ project/lib/api.rb
@@ -1,2 +1,16 @@
+require "pry"
+require 'net/http'
+require 'open-uri'
+require 'json'
+
 class API
+  
+  URL = "https://developers.themoviedb.org/3/genres/get-movie-list"
+  
+ def url 
+  uri = URI.parse(URL)
+    response = Net::HTTP.get_response(uri)
+    response.body
+  end 
+  
 end 
