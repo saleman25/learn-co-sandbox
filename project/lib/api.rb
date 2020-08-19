8cad6a3262c72053bcc32b2aeca5568ffa642088
Samantha Aleman <samantha.aleman25@gmail.com>
2020-08-19 00:15:29 +0000

    added more code to retrieve api

--- project/lib/api.rb
+++ project/lib/api.rb
@@ -6,11 +6,19 @@ require 'json'
 class API
   
   URL = "https://developers.themoviedb.org/3/genres/get-movie-list"
+ 
+  def initalize(url)
+   URL 
+  end 
   
- def url 
+  def url 
   uri = URI.parse(URL)
     response = Net::HTTP.get_response(uri)
     response.body
   end 
+  
+  def parse_json 
+  JSON.parse(get_response_body)
+  end  
   
 end 
